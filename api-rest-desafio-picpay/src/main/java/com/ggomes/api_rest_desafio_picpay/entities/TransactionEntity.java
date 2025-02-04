b6f3874a76dc278d165b52b989d138f99e5de0bd
Guilherme Gomes <ggomes.jua.ti@gmail.com>
2025-02-04 20:40:26 -0300

    :sparkles: feat: Adding entity -> TransactionEntity and Enum TransactionStatus

---
+++ api-rest-desafio-picpay/src/main/java/com/ggomes/api_rest_desafio_picpay/entities/TransactionEntity.java
@@ -0,0 +1,49 @@
+package com.ggomes.api_rest_desafio_picpay.entities;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "transactions")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TransactionEntity {
+	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Double amount;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private TransactionStatus status;
+
+    @ManyToOne
+    @JoinColumn(name = "payer_id", nullable = false)
+    private UserEntity payer;
+
+    @ManyToOne
+    @JoinColumn(name = "payee_id", nullable = false)
+    private UserEntity payee;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
---
+++ api-rest-desafio-picpay/src/main/java/com/ggomes/api_rest_desafio_picpay/entities/TransactionStatus.java
@@ -0,0 +1,7 @@
+package com.ggomes.api_rest_desafio_picpay.entities;
+
+public enum TransactionStatus {
+    PENDING,
+    COMPLETED,
+    FAILED
+}
