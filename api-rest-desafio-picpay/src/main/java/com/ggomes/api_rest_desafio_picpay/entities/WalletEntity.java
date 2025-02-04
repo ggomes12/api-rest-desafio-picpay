c8690118d73e6ccedd2881e3d359c55519484e7d
Guilherme Gomes <ggomes.jua.ti@gmail.com>
2025-02-04 20:33:13 -0300

    :sparkles: feat: Adding entity -> WalletEntity

---
+++ api-rest-desafio-picpay/src/main/java/com/ggomes/api_rest_desafio_picpay/entities/WalletEntity.java
@@ -0,0 +1,34 @@
+package com.ggomes.api_rest_desafio_picpay.entities;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "wallets")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class WalletEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity user;
+
+    @Column(nullable = false)
+    private Double balance;
+}
