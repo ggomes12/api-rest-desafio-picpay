98bcc4ba371ae4d976fcca07ac1dea865b40d647
Guilherme Gomes <ggomes.jua.ti@gmail.com>
2025-02-04 19:46:26 -0300

    :tada: init: First commit with project initial structure

---
+++ api-rest-desafio-picpay/src/main/java/com/ggomes/api_rest_desafio_picpay/ApiRestDesafioPicpayApplication.java
@@ -0,0 +1,13 @@
+package com.ggomes.api_rest_desafio_picpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestDesafioPicpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestDesafioPicpayApplication.class, args);
+	}
+
+}
---
+++ api-rest-desafio-picpay/src/test/java/com/ggomes/api_rest_desafio_picpay/ApiRestDesafioPicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggomes.api_rest_desafio_picpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestDesafioPicpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
