d456eab29c43b1209589ed4f48a1f32f02c4baab
Guilherme Gomes <ggomes.jua.ti@gmail.com>
2025-02-04 20:19:35 -0300

    :sparkles: feat: Adding entity -> UserEntity and Enum UserType

---
+++ api-rest-desafio-picpay/src/main/java/com/ggomes/api_rest_desafio_picpay/entities/UserEntity.java
@@ -0,0 +1,43 @@
+package com.ggomes.api_rest_desafio_picpay.entities;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column(nullable = false, unique = true, length = 20)
+    private String cpfCnpj;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+    
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserType type;
+}
---
+++ api-rest-desafio-picpay/src/main/java/com/ggomes/api_rest_desafio_picpay/entities/UserType.java
@@ -0,0 +1,6 @@
+package com.ggomes.api_rest_desafio_picpay.entities;
+
+public enum UserType {
+    COMMON,
+    MERCHANT
+}
